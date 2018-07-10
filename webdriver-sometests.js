d9534ccc59bcb195de623411cd80fe53484dacd7
Alena Pahuda <Alena_Pahuda@epam.com>
2018-07-10 14:17:34 +0300

    change lot of functions

--- webdriver-sometests.js
+++ webdriver-sometests.js
@@ -1,36 +1,55 @@
-var webdriver = require("selenium-webdriver");
+const webdriver = require("selenium-webdriver");
+const until = webdriver.until;
 
-//var assert = require('chai.assert');
+// yes my own login and password and i dont like it too but had no better idea
+const login = "elena@pogudo.org";
+const password = 'Gfhjkm7244';
+const searchText = "Star";
 
 function createDriver() {
-    var driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(webdriver.Capabilities.chrome()).build();
+    const driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(webdriver.Capabilities.chrome()).build();
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(20000);
     return driver;
 }
 
-var browser = createDriver();
-
-function clickLink(link) {
-    link.click();
-}
+const browser = createDriver();
 
 function handleFailure(err) {
     console.error('Something went wrong\n', err.stack, '\n');
     closeBrowser();
 }
 
-function signIn(login, password) {
-    return browser.findElement(webdriver.By.css('a.btn.head-btn')).click().then(()=>{
-    return browser.findElement(webdriver.By.name('login')).sendKeys(login)}).then(() => {
+function logIn(login, password) {
+    return browser.findElement(webdriver.By.xpath('/html/body/div[@class=\'wrapper\']/header[@class=\'header\']/div[@class=\'header_line\']/div[@class=\'headblock headblock-menu\']/a[@class=\'btn head-btn\'][1]')).click().then(() => {
+        return browser.findElement(webdriver.By.name('login')).sendKeys(login)
+    }).then(() => {
         return browser.findElement(webdriver.By.name('password')).sendKeys(password);
     }).then(() => {
         return browser.findElement(webdriver.By.css('div.loginbox-login > button.btn')).click();
     })
 }
 
+function searchByName(searchQuery) {
+    return browser.findElement(webdriver.By.name("q")).then((el) => {
+        el.clear();
+        return el.sendKeys(searchQuery);
+    }).then(() => {
+        return browser.findElement(webdriver.By.xpath("/html/body/div[@class='wrapper']/header[@class='header']/div[@class='header_line']/form[@id='hsearch']/button[@class='btn head-btn']")).click();
+    }).then(() => {
+        return browser.wait(until.elementLocated(webdriver.By.css("div.pgs-search-title")), 2000).then((el) => {
+            logSearchQuery(el);
+        });
+    })
+}
+
 function closeBrowser() {
     browser.quit();
+}
+
+function previousPage() {
+    browser.navigate().back();
+
 }
 
 function logTitle() {
@@ -39,33 +58,27 @@ function logTitle() {
     });
 }
 
-function logResult(result) {
-    console.log('Consequences: ' + result.length);
+function logSearchQuery(element) {
+    element.getText().then(function (text) {
+        console.log(text);
+    });
 }
 
-
 browser.get('http://www.seasonvar.ru/').then(() => {
-    signIn('elena@pogudo.org','Gfhjkm7244');
-}).then(()=>{
-    return browser.findElement(webdriver.By.name("q"));
-}).then((el) => {
-    browser.sleep(200);
-    el.clear();
-    return el.sendKeys('Star');
-}).then(()=>{
-    browser.sleep(200);
-    return browser.findElement(webdriver.By.css("button.btn.head-btn")).click();
+    logIn(login, password);
 }).then(() => {
-    browser.sleep(200);
-    var result = browser.findElements(webdriver.By.xpath("//a[contains(text(),'Звездные врата')]"));
-    return result;
-}).then((result) => {
-    browser.sleep(200);
-    return logResult(result) + logTitle();
+    searchByName(searchText);
 }).then(() => {
-    browser.navigate().back();
+    previousPage();
+}).then(() => {
+    browser.findElement(webdriver.By.linkText("смотреть сериалы")).click();
+}).then(() => {
+    browser.findElement(webdriver.By.className("header_icon-vkontakte")).click();
     return logTitle();
-    //console.log(assert.equal('Сериалы ТУТ! Сериалы онлайн смотреть бесплатно. Смотреть онлайн','Сериалы ТУТ! Сериалы онлайн смотреть бесплатно. Смотреть онлайн'));
+}).then(() => {
+    previousPage();
 }).then(() => {
     closeBrowser();
+}).catch((err) => {
+    handleFailure(err);
 });
